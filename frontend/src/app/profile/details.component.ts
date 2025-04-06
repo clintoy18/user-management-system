f30b225a9e6aef9cd575ce3079745b9d710a59a2
renzo0314 <lorenzlordtoring@gmail.com>
2025-04-07 07:40:08 +0800

    Implement profile management, admin dashboard, and fake backend

---
+++ frontend/src/app/profile/details.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '@app/_services';
+
+@Component({ templateUrl: 'details.component.html' })
+export class DetailsComponent implements OnInit {
+    account: any; // Replace `any` with the correct type if known
+
+    constructor(private accountService: AccountService) { }
+
+    ngOnInit() {
+        this.account = this.accountService.accountValue;
+    }
+}
+    
