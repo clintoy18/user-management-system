8136f5070618d24049f8cec27b1923d65a09a2d2
clintoy18 <aicsalonzo@gmail.com>
2025-05-13 14:05:45 +0800

    test without emial confimation

--- frontend/src/app/_helpers/fake-backend.ts
+++ frontend/src/app/_helpers/fake-backend.ts
@@ -106,7 +106,7 @@ export class FakeBackendInterceptor implements HttpInterceptor {
 
         function authenticate() {
             const { email, password } = body;
-            const account = accounts.find(x => x.email === email && x.password === password && x.isVerified);
+            const account = accounts.find(x => x.email === email && x.password === password );
         
             if (!account) return error('Email or password is incorrect');
             if (!account.isActive) return error('Account is deactivated');
