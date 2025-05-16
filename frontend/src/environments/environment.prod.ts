be0abc91ccacb9992dae13a90cec08b0f5f38310
Allen Kim <allenkimrafaela@gmail.com>
2025-05-17 02:02:27 +0800

    update apiUrl

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://user-management-system-zqmt.onrender.com'
+    apiUrl: 'https://user-management-system-final-lp9c.onrender.com'
   };
   
